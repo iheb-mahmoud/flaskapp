bbd7fda07053f32f1d9a4e38c6c35cd11d5beacf
houba <138237934+iheb-mahmoud@users.noreply.github.com>
2023-07-04 16:16:04 +0100

    Delete deploy/ansible directory

--- deploy/ansible/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# install ansible, python 3 & pip
-sudo apt update && sudo apt upgrade -y
-sudo apt install -y ansible python3 python3-pip awscli
-# install awscli and boto3 dependencies
-pip3 install --user --upgrade boto3 awscli
-echo 'PATH=$PATH:~/.local/bin' >> ~/.bashrc
-source ~/.bashrc
