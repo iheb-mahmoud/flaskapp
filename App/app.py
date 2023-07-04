7803e823eab2609fde7b42f0932c14d53467ba85
iheb <ihebmahmoud4000@gmail.com>
2023-07-04 16:58:29 +0100

    package app into App folder

---
+++ App/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+import socket
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    server_name = socket.gethostname()
+    container_id = os.uname()[1]
+    return f'Hello from server {server_name}! Container ID: {container_id}'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
