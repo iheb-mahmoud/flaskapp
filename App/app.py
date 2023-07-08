fe4f8b02b0498bb7171ca1b31478cac83c967d27
houba <138237934+iheb-mahmoud@users.noreply.github.com>
2023-07-08 11:27:04 +0100

    Update app.py

--- App/app.py
+++ App/app.py
@@ -1,17 +1,14 @@
-from flask import Flask
-import docker
+import os
 import socket
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    docker_client = docker.DockerClient('unix:///var/run/docker.sock')
-    host_info = docker_client.info()
-    host_name = host_info['Name']
-    container_id = socket.gethostname()
+    host_name = socket.gethostname()
+    container_id = os.uname()[1]
     return f'Hello from server {host_name}! Container ID: {container_id}'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
